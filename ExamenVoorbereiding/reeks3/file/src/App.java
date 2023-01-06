b925f2061895ebb64d8c1006072af486cb76a68a
AdrenalineGhost <marwan.elk2002@gmail.com>
2023-01-06 14:24:26 +0100

    Reeks3

--- ExamenVoorbereiding/reeks3/file/src/App.java
+++ ExamenVoorbereiding/reeks3/file/src/App.java
@@ -1,19 +1,38 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Set;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Persoon vader = new Persoon("mostafa, elkhallouki");
-        Persoon dochter = new Persoon("safa, elkhallouki");
-        Persoon zoon = new Persoon("iyaas, elkhallouki");
-        vader.zetOpRekening(5000, true); vader.zetOpRekening(1000, false);
-        System.out.println(vader.toString()); 
-        System.out.println(zoon.toString()); 
-        System.out.println(dochter.toString());
-        vader.geefZakgeldAan(zoon, 70);
-        vader.geefZakgeldAan(dochter, 5.5);
-        vader.geefSpaarcentAan(zoon, 30);
-        vader.geefSpaarcentAan(dochter, 30);
-        vader.versluisNaarSpaar(vader.getSaldoZicht()-600);
-        System.out.println(vader.toString()); 
-        System.out.println(zoon.toString()); 
-        System.out.println(dochter.toString());
+        // Persoon vader = new Persoon("mostafa, elkhallouki");
+        // Persoon dochter = new Persoon("safa, elkhallouki");
+        // Persoon zoon = new Persoon("iyaas, elkhallouki");
+        // vader.zetOpRekening(5000, true); vader.zetOpRekening(1000, false);
+        // System.out.println(vader.toString()); 
+        // System.out.println(zoon.toString()); 
+        // System.out.println(dochter.toString());
+        // vader.geefZakgeldAan(zoon, 70);
+        // vader.geefZakgeldAan(dochter, 5.5);
+        // vader.geefSpaarcentAan(zoon, 30);
+        // vader.geefSpaarcentAan(dochter, 30);
+        // vader.versluisNaarSpaar(vader.getSaldoZicht()-600);
+        // System.out.println(vader.toString()); 
+        // System.out.println(zoon.toString()); 
+        // System.out.println(dochter.toString());
+
+        ArrayList<String[]> s = new ArrayList<>();
+        ArrayList<Persoon> p = new ArrayList<>();
+        Scanner sc = new Scanner(new File("C:\\Users\\Marwa\\GIT\\Projects\\ExamenVoorbereiding\\reeks3\\file\\src\\personenMetGeld.csv"));
+        while (sc.hasNextLine()) {
+            s.add(sc.nextLine().split(";"));
+        }
+        s.forEach(x -> p.add(
+            new Persoon(x[0])
+                .zetOpRekening(Double.parseDouble(x[1]), false)
+                .zetOpRekening(Double.parseDouble(x[2]), true)
+        ));
+        p.forEach(System.out::println);
     }
 }
--- ExamenVoorbereiding/reeks3/file/src/Persoon.java
+++ ExamenVoorbereiding/reeks3/file/src/Persoon.java
@@ -4,13 +4,13 @@ public class Persoon {
     private Rekening zichtRekening = new Rekening(0);
     private Rekening spaarRekening = new Rekening(0);
     public Persoon(String s){
-        this.voornaam = s.split(",")[0].trim();
-        this.naam = s.split(",")[1].trim();
+        this.naam = s.split(",")[0].trim();
+        this.voornaam = s.split(",")[1].trim();
     }
     double getSaldoZicht(){return zichtRekening.getSaldo();}
     double getVermogen(){return zichtRekening.getSaldo() + spaarRekening.getSaldo();}
     void versluisNaarSpaar(double bedrag){zichtRekening.schrijfOverNaar(spaarRekening, bedrag<0?0:bedrag);}
-    void zetOpRekening(double bedrag, Boolean s){if(!s){zichtRekening.stort(bedrag);}else{spaarRekening.stort(bedrag);}}
+    Persoon zetOpRekening(double bedrag, boolean s){if(!s){zichtRekening.stort(bedrag);}else{spaarRekening.stort(bedrag);}; return this;}
     void geefZakgeldAan(Persoon P, double bedrag){zichtRekening.schrijfOverNaar(P.zichtRekening, bedrag);}
     void geefSpaarcentAan(Persoon P, double bedrag){zichtRekening.schrijfOverNaar(P.spaarRekening, bedrag);}
     @Override
