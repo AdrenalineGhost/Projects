96509c7c92a175e7f02ad825212b2bc2d9800ffc
AdrenalineGhost <marwan.elk2002@gmail.com>
2023-01-21 12:14:48 +0100

    removed *.exe and changes to readme

--- c/oef/oef2.c
+++ c/oef/oef2.c
@@ -3,11 +3,9 @@
 
 int main(){
 
-for (int i = 0; i <= 64; i++)
-{
-printf("%d\t%o\t%x\n",i,i,i);
-}
-
-
-return 0;
+	for (int i = 0; i <= 64; i++)
+	{
+		printf("%d\t%o\t%x\n",i,i,i);
+	}
+	return 0;
 }
