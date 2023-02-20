a8cc7c29d2c76eb4dafd48adb918c0b320d4bc81
AdrenalineGhost <marwan.elk2002@gmail.com>
2023-02-20 09:09:24 +0100

    software labo 1

---
+++ software/labo1/labo1.1/labo1.1/Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labo1._1
+{
+    internal class Student:Person
+    {
+        public Student(string name) : base(name) { }
+        internal string welcomeMessage { get { return "Fuck"} }
+    }
+}
