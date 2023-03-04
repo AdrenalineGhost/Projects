555fa07659a9bf31c2ae5f531ee325550a2e4f7a
AdrenalineGhost <marwan.elk2002@gmail.com>
2023-03-04 21:11:38 +0100

    AOC day4 part1 done

--- AdventOfCode2022/day4/process.py
+++ AdventOfCode2022/day4/process.py
@@ -1,4 +1,4 @@
-ranges = [range(1), range(1)]
+ranges = [0,0]
 count = 0
 
 with open("input.txt","r") as file:
@@ -6,16 +6,18 @@ with open("input.txt","r") as file:
 
 for line in lines:
     line = line.split(",")
-    ranges[0] = range(int((line[0].split("-"))[0]) , (int((line[0].split("-"))[1])+1))
-    ranges[1] = range(int((line[1].split("-"))[0]) , (int((line[1].split("-"))[1])+1))
+    ranges[0] = range(int((line[0].split("-"))[0]) , (int((line[0].split("-"))[1].strip())+1))
+    ranges[1] = range(int((line[1].split("-"))[0]) , (int((line[1].split("-"))[1].strip())+1))
 
-    for rang in range(1):
-        check = [True , True]
+    check = [True , True]
+    for rang in (0,1):
         for index in ranges[rang]:
-            if index not in ranges[~(rang)]:
+            if index not in ranges[0 if rang else 1]:
                 check[rang] = False
     print(line)
-    if check[0] is True or check[1] is False:
+    print(f"--> {ranges[0]}\t{ranges[1]}") 
+    print(f"--> {check[0]}\t\t{check[1]}")
+    if check[0] is True or check[1] is True:
         count += 1
         print("true\n")
     else:
