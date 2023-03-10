39ba0783fc6aa4656392165bf66bd03df3b8ae75
AdrenalineGhost <marwan.elk2002@gmail.com>
2023-03-10 12:01:03 +0100

    fuck rxjs and rxjs-compat

--- gebruiks/labo3/startcode/scripts/main.js
+++ gebruiks/labo3/startcode/scripts/main.js
@@ -1,28 +1,17 @@
 import {stockAlphabet, stockAlphabetWithNullValues} from "./stock.js";
 import {Plot} from "./plot.js";
 const {map, filter} = rxjs.operators;
-const {Observable} = rxjs;
+import { from, Observable, internal, fromEvent } from "node_modules/rxjs";
 const DOLARTOEURO = 0.94;
 
 window.onload = function () {
-    let plot1 = new Plot("Dolar");
-    stockAlphabet
-        .pipe(map(x=>[new Date(x[0]), x[1]]))
-        .subscribe(x=>plot1.plotData(x));
 
-    let plot2 = new Plot("Euro");
-    stockAlphabet
-        .pipe(map(x=>[new Date(x[0]), x[1]*DOLARTOEURO]))
-        .subscribe(x=>plot2.plotData(x));
-
-    let plot3 = new Plot("null(in dolar)");
-    stockAlphabetWithNullValues
-        .pipe(
-            filter(x=>x[0]!=null),
-            filter(x=>x[1]!=null),
-            map(x=>[new Date(x[0]), x[1]])
-        ).subscribe(x=>plot3.plotData(x))
-
+    currency = document.getElementById("currency");
+    stock = document.getElementById("stock");
+    currencyOb = Observable.fromEvent(currency, "click");
+    currencyOb.subscribe(
+        (event)=>console.log(event)
+    )
 
 }
 
