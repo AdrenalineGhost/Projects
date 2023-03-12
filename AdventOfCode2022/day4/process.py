26bc047e55424f8347242f3d5cb54f1b7479e592
AdrenalineGhost <marwan.elk2002@gmail.com>
2023-03-12 19:48:00 +0100

    fuck the nodes packages

--- AdventOfCode2022/day4/process.py
+++ AdventOfCode2022/day4/process.py
@@ -9,18 +9,25 @@ for line in lines:
     ranges[0] = range(int((line[0].split("-"))[0]) , (int((line[0].split("-"))[1].strip())+1))
     ranges[1] = range(int((line[1].split("-"))[0]) , (int((line[1].split("-"))[1].strip())+1))
 
-    check = [True , True]
-    for rang in (0,1):
-        for index in ranges[rang]:
-            if index not in ranges[0 if rang else 1]:
-                check[rang] = False
-    print(line)
-    print(f"--> {ranges[0]}\t{ranges[1]}") 
-    print(f"--> {check[0]}\t\t{check[1]}")
-    if check[0] is True or check[1] is True:
+    # check = [True , True]
+    # for rang in (0,1):
+    #     for index in ranges[rang]:
+    #         if index not in ranges[0 if rang else 1]:
+    #             check[rang] = False
+    # print(line)
+    # print(f"--> {ranges[0]}\t{ranges[1]}")
+    # print(f"--> {check[0]}\t\t{check[1]}")
+    # if check[0] is True or check[1] is True:
+    #     count += 1
+    #     print("true\n")
+    # else:
+    #     print("false\n")
+
+    check = False;
+    for number in ranges[0]:
+        if number in ranges[1]:
+            check = True
+    if check:
         count += 1
-        print("true\n")
-    else:
-        print("false\n")
 
 print(count)
--- gebruiks/labo3/startcode/scripts/main.js
+++ gebruiks/labo3/startcode/scripts/main.js
@@ -1,17 +1,52 @@
-import {stockAlphabet, stockAlphabetWithNullValues} from "./stock.js";
-import {Plot} from "./plot.js";
-//import * as rxjs from "rxjs";
-var Rx = require('rx');
-const DOLARTOEURO = 0.94;
+import { stockAlphabet, stockAlphabetWithNullValues, stockMeta, stockTesla } from "./stock.js";
+import { Plot } from "./plot.js";
+const {map, filter } = rxjs.operators;
+const { Observable, fromEvent } = rxjs;
+
+const stockmap = new Map([
+    ["dolar" , 0.94],
+    ["euro" , 1],
+    ["pound" , 0.88]
+]);
+const currencymap = new Map([
+    ["tesla" , stockTesla],
+    ["meta" , stockMeta],
+    ["alpha" , stockAlphabet]
+]);
+
+
+let currentStock = stockAlphabet;
+let currentCurrency = 1;
 
 window.onload = function () {
+    let plot = new Plot("Stock value");
+
+    rxjs.fromEvent(document.getElementById("top-level-container"), "click")
+        .pipe(()=>{
+            console.log(currentStock);
+            console.log(currentCurrency);
+            currentStock.pipe(
+                map(x=>[new Date(x[0]), x[1] * currentCurrency]
+            )).subscribe(x=>plot.plotData(x));
+        })
+    currentStock.pipe(
+        map(x=>[new Date(x[0]), x[1] * currentCurrency]
+    )).subscribe(x=>plot.plotData(x));
+
 
     currency = document.getElementById("currency");
     stock = document.getElementById("stock");
-    currencyOb = Observable.fromEvent(currency, "click");
-    currencyOb.subscribe(
-        (event)=>console.log(event)
-    )
+
+    currency = rxjs.fromEvent(currency, "click");
+    currency.subscribe(event=>{
+        currentCurrency = currencymap.get(event.target.value);
+    })
+
+    stock = rxjs.fromEvent(stock, "click");
+    stock.subscribe((event)=>{
+        currentStock = stockmap.get(event.target.value);
+    })
+
 
 }
 
