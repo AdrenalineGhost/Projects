68bc9948df6be12a3b2f0f96d5223aa6821ff91e
AdrenalineGhost <marwan.elk2002@gmail.com>
2023-03-28 15:22:14 +0200

    more assembly code stuff -> still doesnt work tho

---
+++ asm/1/write.asm
@@ -0,0 +1,24 @@
+section .text
+    global _start
+
+_start:
+    mov     edx, len
+    mov     ecx, msg
+    mov     ebx, 1
+    mov     eax, 4
+    int     0x80
+
+    mov     edx, 9
+    mov     ecx, s2
+    mov     ebx, 1
+    mov     eax, 4
+    int     0x80
+
+    mov     eax, 1
+    int     0x80
+
+section .data
+
+    msg     db "Display 9 stars", 0xa
+    len     equ $ - msg
+    s2 times 9 db "*"
