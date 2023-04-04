ce477eff65ab15046643223817578d9d0ee5c52a
AdrenalineGhost <marwan.elk2002@gmail.com>
2023-04-04 17:33:40 +0200

    added some comments to the smarthome thing for explaining undefined in ts

--- gebruiks/labo5/smarthome/src/app/notification/notification.component.ts
+++ gebruiks/labo5/smarthome/src/app/notification/notification.component.ts
@@ -6,7 +6,7 @@ import { Component, Input} from '@angular/core';
   styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent{
-  @Input() icon: string|undefined;
-  @Input() boodschap: string|undefined;
+  @Input() icon: string|undefined; // This is vital for making this actually work
+  @Input() boodschap: string|undefined; // without the undefined it doesnt actually work for sonme reason
   timestamp = new Date();
 }
