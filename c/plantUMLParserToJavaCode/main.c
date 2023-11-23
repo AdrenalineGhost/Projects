caaa55e75fc6a01c7ef49dc83bbefaeadbe5be41
AdrenalineGhost <marwan.elk2002@gmail.com>
2023-11-23 13:49:58 +0100

    test on linux

---
+++ c/plantUMLParserToJavaCode/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+
+    FILE *fptr;
+    fptr = fopen("C:\\Users\\Marwa\\git\\Projects\\c\\plantUMLParserToJavaCode\\puml.puml","r");
+    if(fprt == NULL){printf("Not a correct adress!"); return 1;}
+
+    char * fileString;
+    while(fgets(fileString,100,fptr))
+
+
+
+
+
+
+    return 0;
+}
